24355d131f598188eaf55b22cc4f712c2170670c
Duy Tan <ngtanpqkg2020@gmail.com>
2025-05-22 19:06:02 +0700

    fix

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,15 +4,10 @@ import {Routes, Route} from 'react-router-dom';
 import AboutUs from './AboutUs';
 import ContactUs from './ContactUs';
 import DetailProduct from './DetailProduct';
-// <<<<<<< HEAD
 import Blog from './blog';
-// =======
 import DentalHomePage from './Home';
-<<<<<<< HEAD
 import Products from './Products';
-=======
-// >>>>>>> 3062eff1d59323062f7583cc784130a2cc318ce1
->>>>>>> 5156e45e81384460e98c3b6e14d4b32f46a7980e
+
 
 function NotFound (){
     return <h1 className='text-center font-bold'> 404 - Not Found </h1>
@@ -27,7 +22,7 @@ export default function AppRouter() {
         <Route path='/about-us' element={<AboutUs></AboutUs>} ></Route>
         <Route path='/contact-us' element={<ContactUs></ContactUs>} ></Route>
         <Route path='/details' element={<DetailProduct></DetailProduct>} ></Route>
-        <Route path='/blog'  element={<Blog></Blog>} ></Route>
+        <Route path='/blogs'  element={<Blog></Blog>} ></Route>
         <Route path='*' element={<NotFound></NotFound>} ></Route>
 
       </Routes>
