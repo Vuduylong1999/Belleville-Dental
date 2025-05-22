5156e45e81384460e98c3b6e14d4b32f46a7980e
pham-thanh-nhan <phamthanhnhankk123@gmail.com>
2025-05-22 18:48:53 +0700

    em Block

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,4 +1,4 @@
-import React, {useRef} from "react";
+import React, { useRef } from "react";
 import './page.css'
 
 
@@ -15,8 +15,10 @@ export default function Blog() {
 
     const cards = [
         {
-            title: "Care of your Teeth",
+            title: "Bàn chải đánh răng điện",
             author: "Anita Jackson",
+            price: "đ379.000",
+            mota: "Giúp làm sạch răng hiệu quả với công nghệ rung sóng âm.",
             img: "https://hoanghamobile.com/tin-tuc/wp-content/uploads/2024/04/anh-meo-ngau-43.jpg",
             tag: "Self Care",
         },
@@ -55,7 +57,7 @@ export default function Blog() {
 
     return <>
 
-        
+
         <div className="text-center khung-blogs">
             <p className="blogs mt-5" >
                 Blogs
@@ -65,7 +67,7 @@ export default function Blog() {
             </p>
         </div>
         <div className="container py-5 mt-5">
-            
+
 
             <div
                 className="d-flex overflow-auto gap-3 px-2"
@@ -79,8 +81,12 @@ export default function Blog() {
                             <span className="badge bg-primary mb-2">{card.tag}</span>
                             <h5 className="card-title">{card.title}</h5>
                             <p className="card-text text-muted">
-                                Lorem ipsum dolor sit amet consectetur.
+                                {card.mota}
                             </p>
+                            <div class="mt-1 d-flex justify-between items-center gap-3 align-items-center">
+                                <span class="text-danger font-bold text-lg fw-bold">{card.price}</span>
+                                <button class=" s px-4 py-2 mua-ngay-blog">Mua ngay</button>
+                            </div>
                             <p className="text-end text-muted mb-0">
                                 ~{card.author}
                             </p>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,11 +4,11 @@ import {Routes, Route} from 'react-router-dom';
 import AboutUs from './AboutUs';
 import ContactUs from './ContactUs';
 import DetailProduct from './DetailProduct';
-<<<<<<< HEAD
+// <<<<<<< HEAD
 import Blog from './blog';
-=======
+// =======
 import DentalHomePage from './Home';
->>>>>>> 3062eff1d59323062f7583cc784130a2cc318ce1
+// >>>>>>> 3062eff1d59323062f7583cc784130a2cc318ce1
 
 function NotFound (){
     return <h1 className='text-center font-bold'> 404 - Not Found </h1>
@@ -22,8 +22,8 @@ export default function AppRouter() {
         <Route path='/about-us' element={<AboutUs></AboutUs>} ></Route>
         <Route path='/contact-us' element={<ContactUs></ContactUs>} ></Route>
         <Route path='/details' element={<DetailProduct></DetailProduct>} ></Route>
-        <Route path='*' element={<NotFound></NotFound>} ></Route>
         <Route path='/blog'  element={<Blog></Blog>} ></Route>
+        <Route path='*' element={<NotFound></NotFound>} ></Route>
 
       </Routes>
     </>
